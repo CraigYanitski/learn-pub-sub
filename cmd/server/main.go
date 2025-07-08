e6e2070d072cb57306f6965f647a11881290a782
yanitski <yanitski@ph1.uni-koeln.de>
2025-07-07 20:08:14 -0600

    connect to rabbitmq broker

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+    defer fmt.Println("Peril server shutting down")
+
+    rabbitStr := "amqp://guest:guest@localhost:5672/"
+    conn, err := amqp.Dial(rabbitStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+    defer fmt.Println("\nDisconnected from broker")
+    fmt.Println("Connection successful")
+
+    sigs := make(chan os.Signal, 1)
+    signal.Notify(sigs, syscall.SIGINT)
+    <-sigs
 }
